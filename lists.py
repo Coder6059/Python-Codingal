c60010921dae55e272068952129b8ceb5b1b9a39
Coder6059 <ultimteking605@gmail.com>
2026-04-21 18:38:53 +0530

    list

---
+++ lists.py
@@ -0,0 +1,12 @@
+empty_list = []
+print(empty_list)
+
+numbers = [1,2,3,4,5,6]
+print(numbers)
+
+triples = [1,10,5] * 3
+print(triples)
+
+list1 = [15,30,45,60,75]
+list1 = list1[::-1]
+print(list1)
