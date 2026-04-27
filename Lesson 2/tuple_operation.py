3065ff43bca063fdea5aacbc2183d715597e93dc
Coder6059 <ultimteking605@gmail.com>
2026-04-27 18:55:20 +0530

    lesson 2

---
+++ Lesson 2/tuple_operation.py
@@ -0,0 +1,18 @@
+tuple1 = ("code", 2, 4.9, True)
+print(tuple1)
+
+tuple2 = (1,2,5,7,3,9,5)
+print(tuple2)
+
+tuple3 = tuple2 + (10, 20, 30)
+print(tuple3)
+
+tuple4 = (1,3,3,6,2,3,9,3)
+print(tuple4.count(3))
+
+tuple5 = (1,2,3,4,5,6,7,8,9,10)
+slice1 = tuple5[3:7]
+print(slice1)
+
+slice2 = tuple5[:6]
+print(slice2)
--- README.md
+++
@@ -1,1 +0,0 @@
-Whatever I code with the help of interface Codingal, will be stored in this public repository. Hope u enjoy :) 
