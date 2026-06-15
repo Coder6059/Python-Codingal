a9a363779dbe74274588c1ac57483c63583fdb1d
Coder6059 <ultimteking605@gmail.com>
2026-06-15 17:53:07 +0530

    lesson 1

--- Module 4/Lesson 1/dog_breed.py
+++ Module 4/Lesson 1/dog_breed.py
@@ -12,4 +12,4 @@ ob1 = dog_breed("Golden Retriever", "Medium", 1000, 3)
 ob2 = dog_breed("Poodle", "Small", 500, 1)
 
 print("The first dog's breed is ", ob1.breed, ", its size is ", ob1.size, ", it costs ", ob1.price, ", and is ", ob1.age, "year(s) old")
-print("The second dog's breed is ", ob2.breed, ", its size is ", ob2.size, ", it costs ", ob2.price, ", and is ", ob2.age, "year(s) old")
+print("The second dog's breed is ", ob2.breed, ", its size is ", ob2.size, ", it costs ", ob2.price, ", and is ", ob2.age, "year(s) old")   
